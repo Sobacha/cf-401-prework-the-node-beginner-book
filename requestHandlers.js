8bf71e440730e0441d8081493524e19b9a8e5238
Rima <rmhrok@gmail.com>
2018-01-13 04:08:31 +0000

    use querystring to get only value of text field

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,3 +1,5 @@
+var queryString = require("querystring");
+
 function start(response, postData){
 	console.log("Request handler 'start' was called.");
 
@@ -22,7 +24,7 @@ function start(response, postData){
 function upload(response, postData){
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type" : "text/plain"});
-	response.write("You've sent: " + postData);
+	response.write("You've sent the text: " + queryString.parse(postData).text);
 	response.end();
 }
 
